baf7cc608b15ba56f38d9c0d84777522cb1ad131
James <jgreenop22@gmail.com>
2025-06-17 23:30:26 -0700

    rename main class

--- src/main/java/g_mungus/genesis/GenesisMod.java
+++ src/main/java/g_mungus/genesis/GenesisMod.java
@@ -2,11 +2,11 @@ package g_mungus.genesis;
 
 import net.neoforged.fml.common.Mod;
 
-@Mod(ExampleMod.MOD_ID)
-public final class ExampleMod {
+@Mod(GenesisMod.MOD_ID)
+public final class GenesisMod {
     public static final String MOD_ID = "genesis";
 
-    public ExampleMod() {
+    public GenesisMod() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like registries and resources) may still be uninitialized.
         // Proceed with mild caution.
