1eadc37464d613137e106f519a218ce22ec4f6fc
James <jgreenop22@gmail.com>
2025-06-18 16:22:43 -0700

    scale movement

--- src/main/java/g_mungus/genesis/mixin/EntityMixin.java
+++ src/main/java/g_mungus/genesis/mixin/EntityMixin.java
@@ -2,11 +2,16 @@ package g_mungus.genesis.mixin;
 
 import g_mungus.genesis.GenesisMod;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -18,17 +23,36 @@ public abstract class EntityMixin {
 
     @Shadow public abstract Level level();
 
+    @Unique
+    private boolean genesis$isInSpace() {
+        return level.dimension().location().equals(GenesisMod.SPACE_DIM);
+    }
+
     @Inject(method = "onBelowWorld", at = @At("HEAD"), cancellable = true)
     private void onBelowWorldMixin(CallbackInfo ci) {
-        if(level.dimension().location().equals(GenesisMod.SPACE_DIM)) {
+        if(genesis$isInSpace()) {
             ci.cancel();
         }
     }
 
     @Inject(method = "isNoGravity", at = @At("HEAD"), cancellable = true)
     public void isNoGravityMixin(CallbackInfoReturnable<Boolean> cir) {
-        if (level.dimension().location().equals(GenesisMod.SPACE_DIM)) {
+        if (genesis$isInSpace()) {
             cir.setReturnValue(true);
         }
     }
+
+    @ModifyVariable(
+            method = "move(Lnet/minecraft/world/entity/MoverType;Lnet/minecraft/world/phys/Vec3;)V",
+            at = @At("HEAD"),
+            index = 2, // 0 = 'this', 1 = MoverType, 2 = Vec3
+            argsOnly = true
+    )
+    private Vec3 modifyMoveVector(Vec3 vec3, MoverType type) {
+        if (genesis$isInSpace() && (type == MoverType.SELF || type == MoverType.PLAYER)) {
+            return vec3.scale(1.0 / 16.0);
+        }
+        return vec3;
+    }
+
 }
