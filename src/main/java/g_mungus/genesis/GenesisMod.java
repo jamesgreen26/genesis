c65c5e71723bdb78d079c3913713f8474b49e214
James <jgreenop22@gmail.com>
2025-06-18 14:37:40 -0700

    ship gravity in space

--- src/main/java/g_mungus/genesis/GenesisMod.java
+++ src/main/java/g_mungus/genesis/GenesisMod.java
@@ -1,10 +1,20 @@
 package g_mungus.genesis;
 
 import g_mungus.genesis.dimension.GreatUnknownDimension;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import org.joml.Vector3d;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
+@EventBusSubscriber
 @Mod(GenesisMod.MOD_ID)
 public final class GenesisMod {
     public static final String MOD_ID = "genesis";
@@ -16,6 +26,15 @@ public final class GenesisMod {
         // However, some things (like registries and resources) may still be uninitialized.
         // Proceed with mild caution.
         eventBus.addListener(GreatUnknownDimension::registerEffects);
+    }
 
+    @SubscribeEvent
+    public static void onLevelStartup(LevelEvent.Load event) {
+        LevelAccessor level = event.getLevel();
+        if (level instanceof ServerLevel serverLevel) {
+            ResourceKey<Level> dimension = serverLevel.dimension();
+            if (dimension.location().equals(SPACE_DIM))
+                VSGameUtilsKt.getShipObjectWorld(serverLevel).updateDimension(dimension.registry() + ":" + dimension.location(), new Vector3d());
+        }
     }
 }
