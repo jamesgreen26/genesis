071e57573be574b8824878085d67ae01ad8bbbc2
James <jgreenop22@gmail.com>
2025-06-20 15:13:49 -0700

    custom break particles

--- src/main/java/g_mungus/genesis/mixin/ParticleEngineMixin.java
+++ src/main/java/g_mungus/genesis/mixin/ParticleEngineMixin.java
@@ -8,9 +8,11 @@ import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -26,10 +28,6 @@ public class ParticleEngineMixin {
         ClientLevel level = Minecraft.getInstance().level;
         if (level != null && level.dimension().location().equals(GenesisMod.SPACE_DIM)) {
 
-            if (arg.getType().equals(ParticleTypes.BLOCK)) {
-                cir.setReturnValue(null);
-            }
-
             Particle particle = this.makeParticle(arg, d, e, f, g, h, i);
             if (particle != null) {
 
@@ -37,7 +35,7 @@ public class ParticleEngineMixin {
                     return;
                 }
 
-                particle.scale(1/16f);
+                particle.scale(1 / 16f);
                 this.add(particle);
                 cir.setReturnValue(particle);
             } else {
@@ -49,7 +47,20 @@ public class ParticleEngineMixin {
     @Inject(method = "destroy", at = @At("HEAD"), cancellable = true)
     public void destroyMixin(BlockPos arg, BlockState arg2, CallbackInfo ci) {
         ClientLevel level = Minecraft.getInstance().level;
-        if (level == null || level.dimension().location().equals(GenesisMod.SPACE_DIM)) {
+        if (level != null && level.dimension().location().equals(GenesisMod.SPACE_DIM)) {
+
+            for (int i = 0; i < 10; i++) {
+                level.addParticle(
+                        new BlockParticleOption(ParticleTypes.BLOCK, arg2),
+                        arg.getX() + Math.random(),
+                        arg.getY() + Math.random(),
+                        arg.getZ() + Math.random(),
+                        0,
+                        0,
+                        0
+                );
+            }
+
             ci.cancel();
         }
     }
