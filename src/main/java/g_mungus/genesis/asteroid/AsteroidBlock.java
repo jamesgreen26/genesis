7006d3cfd6c33ca2d8151f991ed424a3a9142b1a
James <jgreenop22@gmail.com>
2025-06-21 18:18:54 -0700

    drop resources

--- src/main/java/g_mungus/genesis/asteroid/AsteroidBlock.java
+++ src/main/java/g_mungus/genesis/asteroid/AsteroidBlock.java
@@ -1,16 +1,23 @@
 package g_mungus.genesis.asteroid;
 
 import g_mungus.genesis.asteroid.generation.AsteroidGenerator;
+import g_mungus.genesis.asteroid.generation.composition.CompositionData;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.shapes.*;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -19,6 +26,7 @@ import static g_mungus.genesis.asteroid.generation.AsteroidGenerator.ASTEROID_CO
 public class AsteroidBlock extends Block {
     public static final IntegerProperty INDEX = IntegerProperty.create("index", 0, ASTEROID_COUNT);
     public static final ConcurrentMap<Integer, VoxelShape> asteroidShapes = new ConcurrentHashMap<>();
+    public static final ConcurrentMap<Integer, CompositionData> compositions = new ConcurrentHashMap<>();
 
 
     public AsteroidBlock(Properties arg) {
@@ -43,5 +51,16 @@ public class AsteroidBlock extends Block {
     @Override
     public @NotNull RenderShape getRenderShape(@NotNull BlockState state) {
         return RenderShape.INVISIBLE;
+    }
+
+
+
+    @Override
+    protected List<ItemStack> getDrops(BlockState state, LootParams.Builder arg2) {
+        int index = state.getValue(INDEX);
+
+        CompositionData comp = compositions.getOrDefault(index, new CompositionData());
+
+        return comp.getComposition().stream().map(it -> new ItemStack(BuiltInRegistries.ITEM.get(ResourceLocation.parse(it.getType())), it.getAmount())).toList();
     }
 }
--- src/main/java/g_mungus/genesis/asteroid/generation/composition/CompositionDataLoader.java
+++ src/main/java/g_mungus/genesis/asteroid/generation/composition/CompositionDataLoader.java
@@ -1,4 +1,41 @@
 package g_mungus.genesis.asteroid.generation.composition;
 
+import com.fasterxml.jackson.databind.*;
+import g_mungus.genesis.asteroid.AsteroidBlock;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import java.io.InputStream;
+
+import static g_mungus.genesis.asteroid.generation.AsteroidGenerator.ASTEROID_COUNT;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class CompositionDataLoader {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    @SubscribeEvent
+    public static void loadAllFromDisk(FMLCommonSetupEvent event) {
+        for (int i = 0; i < ASTEROID_COUNT; i++) {
+            try {
+                CompositionData compositionData = load("asteroid/composition/asteroid_" + i + ".json");
+                AsteroidBlock.compositions.put(i, compositionData);
+            } catch (Exception e) {
+                System.out.println("failed to load composition for asteroid: " + i);
+            }
+        }
+    }
+
+    static CompositionData load(String filename) {
+
+        try (InputStream stream = CompositionDataLoader.class.getResourceAsStream("/" + filename)) {
+            if (stream == null) {
+                throw new IllegalStateException("Couldn't find resource file!");
+            }
+
+            return MAPPER.readValue(stream, CompositionData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load static JSON data", e);
+        }
+    }
 }
