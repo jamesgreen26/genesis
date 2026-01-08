4ae79c3e3caf6c084d24097af2682d8d7e5437dd
James Green <jgreenop22@gmail.com>
2026-01-08 00:57:06 -0800

    fix builtin datapack & fix crash

--- src/main/java/shipwrights/genesis/content/block/AsteroidBlock.java
+++ src/main/java/shipwrights/genesis/content/block/AsteroidBlock.java
@@ -16,7 +16,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import org.joml.Vector3d;
-import shipwrights.genesis.mixin.extension.FallingBlockEntityExtension;
+import shipwrights.genesis.mixin_extension.FallingBlockEntityExtension;
 import shipwrights.genesis.mixin.FallingBlockEntityAccessor;
 
 import java.util.List;
--- src/main/java/shipwrights/genesis/mixin/FallingBlockRendererMixin.java
+++ src/main/java/shipwrights/genesis/mixin/FallingBlockRendererMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import shipwrights.genesis.mixin.extension.FallingBlockEntityExtension;
+import shipwrights.genesis.mixin_extension.FallingBlockEntityExtension;
 
 @Mixin(FallingBlockRenderer.class)
 public class FallingBlockRendererMixin {
