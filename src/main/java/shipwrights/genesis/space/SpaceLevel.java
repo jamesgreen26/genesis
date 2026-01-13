0c57cf5b63beab6c285012997511860904f06015
James Green <jgreenop22@gmail.com>
2026-01-12 20:16:57 -0800

    refactor raycast

--- src/main/java/shipwrights/genesis/space/SpaceLevel.java
+++ src/main/java/shipwrights/genesis/space/SpaceLevel.java
@@ -13,6 +13,8 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+import static shipwrights.genesis.math.Raycast.raycastOBB;
+
 public class SpaceLevel {
 
     public static Optional<OrbitingBody.WithDistanceSq> getNearestOrbitingBody(Vector3dc position, long ticks) {
@@ -29,54 +31,7 @@ public class SpaceLevel {
                 .filter(pwd -> pwd.getDistanceSquared() < Double.MAX_VALUE);
     }
 
-    private static double raycastAABB(Vector3d origin, Vector3d direction, Vector3d min, Vector3d max)
-    {
-        double tMin = 0.0;
-        double tMax = Double.POSITIVE_INFINITY;
-
-        for (int i = 0; i < 3; i++) {
-            double originA = origin.get(i);
-            double DirectionA = direction.get(i);
-            double minA = min.get(i);
-            double maxA = max.get(i);
-
-            if (Math.abs(DirectionA) < 1e-9) {
-                if (originA < minA || originA > maxA)
-                    return Double.POSITIVE_INFINITY;
-            } else {
-                double invD = 1.0 / DirectionA;
-                double t1 = (minA - originA) * invD;
-                double t2 = (maxA - originA) * invD;
-                if (t1 > t2) {
-                    double tmp = t1;
-                    t1 = t2;
-                    t2 = tmp;
-                }
-                tMin = Math.max(tMin, t1);
-                tMax = Math.min(tMax, t2);
-                if (tMin > tMax)
-                    return Double.POSITIVE_INFINITY;
-            }
-        }
-
-        return tMin;
-    }
-
-    private static double raycastOBB(Vector3d origin, Vector3d direction, Vector3d center, Matrix3d rotation, Vector3d localMin, Vector3d localMax)
-    {
-        // Inverse rotation = transpose (rotation is orthonormal)
-        Matrix3d invRot = new Matrix3d(rotation).transpose();
-
-        // Transform ray into box-local space
-        Vector3d localOrigin = invRot.transform(new Vector3d(origin).sub(center));
-
-        Vector3d localDir = invRot.transform(new Vector3d(direction));
-
-        return raycastAABB(localOrigin, localDir, localMin, localMax);
-    }
-
-    public static Optional<Orbitable.Celestial.WithDistanceSq<Orbitable.Celestial>> celestialRaycast(long ticks, Vector3d origin, Vector3d direction)
-    {
+    public static Optional<Orbitable.Celestial.WithDistanceSq<Orbitable.Celestial>> celestialRaycast(long ticks, Vector3d origin, Vector3d direction) {
         double closestT = Double.POSITIVE_INFINITY;
         Orbitable.Celestial result = null;
 
@@ -105,8 +60,7 @@ public class SpaceLevel {
             }
         }
 
-        if(result!=null)
-        {
+        if(result!=null) {
             return Optional.of(new Orbitable.Celestial.WithDistanceSq<>(result, closestT * closestT));
         }
 
