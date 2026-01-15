9615ed64b9fc80e3ea16a5ebd9e597ac229b2a63
James Green <jgreenop22@gmail.com>
2026-01-14 18:22:51 -0800

    improve test item

--- src/main/java/shipwrights/genesis/content/item/TestItem.java
+++ src/main/java/shipwrights/genesis/content/item/TestItem.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3d;
+import org.joml.Vector3dc;
 import shipwrights.genesis.GenesisMod;
 import shipwrights.genesis.space.SpaceLevel;
 
@@ -28,6 +29,8 @@ public class TestItem extends Item {
             var result = SpaceLevel.celestialRaycast(GenesisMod.getTicks(arg), 0f, origin,direction, celestialType -> true);
             if (result != null) {
                 arg2.sendSystemMessage(Component.literal("BODY FOUND: " + result.getFirst().getID()));
+                Vector3dc pos = result.getFirst().getPosition(GenesisMod.getTicks(arg));
+                arg2.sendSystemMessage(Component.literal("Position: " + (int) pos.x() + " " + (int) pos.y() + " " + (int) pos.z()));
             }
         }
         return super.use(arg, arg2, arg3);
