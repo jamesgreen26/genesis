7383f756ebe0fb83847c41c421781be3e42cefa4
JSJBDEV <acrodynasty@gmail.com>
2026-01-29 15:38:39 +0000

    The test item can now be used to teleport a ship to 0 200 0 in the nether this includes all entities aboard it and contraptions

--- src/main/java/shipwrights/genesis/teleportation/VSUtils.java
+++ src/main/java/shipwrights/genesis/teleportation/VSUtils.java
@@ -1,11 +1,22 @@
 package shipwrights.genesis.teleportation;
 
+import com.mojang.brigadier.ParseResults;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Vector3dc;
+import org.joml.primitives.AABBdc;
 import org.valkyrienskies.core.api.ships.LoadedServerShip;
 import org.valkyrienskies.core.api.ships.QueryableShipData;
+import org.valkyrienskies.core.api.ships.ServerShip;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class VSUtils {
@@ -20,5 +31,27 @@ public class VSUtils {
             }
         });
         return loadedShips;
+    }
+
+    public static void moveShipToDimensionAt(ServerLevel from, ServerShip ship, String dimensionID, Vec3 pos)
+    {
+        CommandSourceStack stack = from.getServer().createCommandSourceStack();
+        Vector3dc joml = ship.getTransform().getPositionInWorld();
+        Vec3 fromPos = new Vec3(joml.x(),joml.y(),joml.z());
+        AABBdc box = ship.getWorldAABB();
+        List<Entity> allToMove = from.getEntitiesOfClass(Entity.class,new AABB(new Vec3(box.minX(),box.minY(),box.minZ()),new Vec3(box.maxX(),box.maxY(),box.maxZ())));
+
+        for(Entity e: allToMove)
+        {
+            Vec3 relative = fromPos.subtract(e.getPosition(0));
+            Vec3 end = pos.add(relative);
+
+            from.getServer().getCommands().performPrefixedCommand(stack,
+                    "execute in "+dimensionID+" run tp "+e.getStringUUID()+" "+end.x()+" "+end.y()+" "+end.z());
+        }
+
+        from.getServer().getCommands().performPrefixedCommand(stack,
+                "execute in "+dimensionID+" run vs teleport "+ship.getSlug()+" "+pos.x()+" "+pos.y()+" "+pos.z());
+
     }
 }
