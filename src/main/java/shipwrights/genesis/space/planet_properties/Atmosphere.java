5ae162a4490d08054e906cd98276187106988d4c
James Green <jgreenop22@gmail.com>
2026-03-16 15:01:36 -0700

    Add "isBreathable" to atmosphere properties

--- src/main/java/shipwrights/genesis/space/planet_properties/Atmosphere.java
+++ src/main/java/shipwrights/genesis/space/planet_properties/Atmosphere.java
@@ -7,12 +7,14 @@ public record Atmosphere(
         double density,
         double thickness,
         boolean precipitation,
+        boolean isBreathable,
         PlanetColorPalette color
 ) {
     public static final Codec<Atmosphere> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.DOUBLE.fieldOf("density").forGetter(Atmosphere::density),
             Codec.DOUBLE.fieldOf("thickness").forGetter(Atmosphere::thickness),
             Codec.BOOL.fieldOf("precipitation").forGetter(Atmosphere::precipitation),
+            Codec.BOOL.fieldOf("isBreathable").forGetter(Atmosphere::isBreathable),
             PlanetColorPalette.CODEC.fieldOf("color").forGetter(Atmosphere::color)
     ).apply(instance, Atmosphere::new));
 }
