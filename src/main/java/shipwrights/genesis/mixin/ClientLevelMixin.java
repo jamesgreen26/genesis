2fd29a826329efcd715ff4e7580ac465981d7cad
James Green <jgreenop22@gmail.com>
2026-03-16 15:33:49 -0700

    More durable mixin target

--- src/main/java/shipwrights/genesis/mixin/ClientLevelMixin.java
+++ src/main/java/shipwrights/genesis/mixin/ClientLevelMixin.java
@@ -1,39 +1,43 @@
 package shipwrights.genesis.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.biome.AmbientParticleSettings;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Optional;
 
 @Mixin(ClientLevel.class)
 public abstract class ClientLevelMixin {
 
-    @WrapOperation(
-            method = "lambda$doAnimateTick$8",
+    @Unique
+    public final RandomSource genesis$random = RandomSource.create();
+
+    @ModifyExpressionValue(
+            method = "doAnimateTick",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/biome/AmbientParticleSettings;canSpawn(Lnet/minecraft/util/RandomSource;)Z"
-            ),
-            remap = false
+                    target = "Lnet/minecraft/world/level/biome/Biome;getAmbientParticle()Ljava/util/Optional;"
+            )
     )
-    private boolean genesis$fadeBiomeParticles(
-            AmbientParticleSettings settings,
-            RandomSource random,
-            Operation<Boolean> original
+    private Optional<AmbientParticleSettings> genesis$fadeBiomeParticles(
+            Optional<AmbientParticleSettings> original
     ) {
-        boolean base = original.call(settings, random);
-        if (!base) {
-            return false;
+        if (original.isEmpty()) {
+            return original;
         }
-
         double camY = Minecraft.getInstance().gameRenderer.getMainCamera().getPosition().y;
         double densityFade = 1.0 - Mth.clamp((camY - 300.0) / 60.0, 0.0, 1.0);
 
-        return densityFade >= 1.0 || random.nextFloat() < densityFade;
+        if (densityFade >= 1.0) {
+            return original;
+        }
+
+        return this.genesis$random.nextFloat() < densityFade ? original : Optional.empty();
     }
 }
