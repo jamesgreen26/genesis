395399474660e004bf80d8aff129ad582b1bb1eb
James Green <jgreenop22@gmail.com>
2026-03-20 13:50:36 -0700

    Fix snow accumulation in space

--- src/main/java/shipwrights/genesis/mixin/ServerLevelMixin.java
+++ src/main/java/shipwrights/genesis/mixin/ServerLevelMixin.java
@@ -1,15 +1,21 @@
 package shipwrights.genesis.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.valkyrienskies.core.api.ships.Ship;
+import org.valkyrienskies.mod.common.VSGameUtilsKt;
 import shipwrights.genesis.GenesisMod;
 
 @Mixin(ServerLevel.class)
@@ -25,5 +31,20 @@ public abstract class ServerLevelMixin {
     @Inject(method = "addPlayer", at = @At("HEAD"))
     private void addPlayerMixin(ServerPlayer arg, CallbackInfo ci) {
         GenesisMod.refreshEntityScaling(arg, getLevel());
+    }
+
+    @WrapOperation(method = "tickChunk", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/biome/Biome;shouldSnow(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;)Z"))
+    private boolean genesis$shouldSnow(Biome instance, LevelReader levelReader, BlockPos pos, Operation<Boolean> original) {
+        ServerLevel level = getLevel();
+        if (GenesisMod.shouldCancelVoidDamage(level)) {
+            return false;
+        }
+        Ship ship = VSGameUtilsKt.getShipManagingPos(level, pos);
+
+        if (ship != null && ship.getTransform().getPositionInWorld().y() > 400) {
+            return false;
+        }
+
+        return original.call(instance, level, pos);
     }
 }
